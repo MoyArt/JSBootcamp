60ee34e9aeca85b672f1f159ee36aa2902874f25
MoyArt Maximus <moyart@gmail.com>
2018-08-14 22:03:38 -0500

    Challenge added to strings.js

--- strings.js
+++ strings.js
@@ -3,4 +3,17 @@ let firstName = 'MoyArt';
 let lastName = 'Maximus';
 let fullName = firstName + ' ' + lastName;
 
-console.log(fullName)
+console.log(fullName);
+
+/* 
+    CHALLENGE: Create 3 variables, city, country and location,
+    location has to be city plus country.
+    At the end, console.log location, it has to return for example:
+    CDMX, Mexico
+*/
+
+let city = 'CDMX';
+let country = 'Mexico';
+let location = city + ', ' + country;
+
+console.log(location);
