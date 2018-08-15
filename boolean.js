7d59761e649e0d958752526013e2021218f5e8a9
MoyArt Maximus <moyart@gmail.com>
2018-08-14 23:04:23 -0500

    Boolean operators

---
+++ boolean.js
@@ -0,0 +1,15 @@
+/*  Boolean operators are true or false values
+    We can declare a value of true or false, or get the result using the next operators
+    === - equality operator
+    !== - not equal operator
+    < - minor than operator
+    > - greater thar operator
+    <= - minor or equal to operator
+    >= - greater or equal to operator
+*/
+
+let temperature = 31;
+
+let isFreezing = temperature <= 32;
+
+console.log(isFreezing);
